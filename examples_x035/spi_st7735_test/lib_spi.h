98c01f7c16b59d1fffe793caf2049d1f65053aff
uni369 <unicabpp@yahoo.com>
2025-11-10 17:45:56 -0500

    debug ST7735.

---
+++ examples_x035/spi_st7735_test/lib_spi.h
@@ -0,0 +1,130 @@
+#include "ch32fun.h"
+
+typedef struct {
+	s16 mosi_pin;
+	s16 miso_pin;
+	s16 sck_pin;
+	s16 cs_pin;
+	s16 dc_pin;
+	s16 rst_pin;
+} SPI_Pins_t;
+
+static void SPI_init(SPI_Pins_t *pins) {
+	funPinMode(pins->mosi_pin, GPIO_CFGLR_OUT_50Mhz_AF_PP);
+	funPinMode(pins->sck_pin, GPIO_CFGLR_OUT_50Mhz_AF_PP);
+
+	if (pins->miso_pin >= 0) {
+		funPinMode(pins->miso_pin, GPIO_CNF_IN_FLOATING);
+	}
+
+	if (pins->cs_pin >= 0) {
+		funPinMode(pins->cs_pin, GPIO_CFGLR_OUT_10Mhz_PP);
+	}
+
+	if (pins->dc_pin >= 0) {
+		funPinMode(pins->dc_pin, GPIO_CFGLR_OUT_10Mhz_PP);
+		funDigitalWrite(pins->dc_pin, 0);
+	}
+	
+	if (pins->rst_pin >= 0) {
+		funPinMode(pins->rst_pin, GPIO_CFGLR_OUT_10Mhz_PP);
+		funDigitalWrite(pins->rst_pin, 0);
+		Delay_Ms(100);
+		funDigitalWrite(pins->rst_pin, 1);
+		Delay_Ms(100);	
+	}
+
+	// Enable GPIO Port C and SPI peripheral
+	RCC->APB2PCENR |= RCC_APB2Periph_SPI1;
+
+	// reset control register
+	SPI1->CTLR1 = 0;
+
+	// Configure SPI
+	SPI1->CTLR1 |= SPI_CPHA_1Edge | SPI_CPOL_Low | SPI_Mode_Master |
+					SPI_BaudRatePrescaler_4 |
+					SPI_NSS_Soft |
+					SPI_DataSize_16b |
+					3<<3; // Divisior = 16 (48/16 = 3MHz)
+
+	SPI1->CTLR1 |= SPI_Direction_2Lines_FullDuplex;
+	// SPI1->CTLR1 |= SPI_Direction_1Line_Tx;
+
+	// Enable SPI Port
+	SPI1->CTLR1 |= CTLR1_SPE_Set;
+}
+
+
+static void SPI_DMA_init(DMA_Channel_TypeDef* DMA_Channel, u32 *DATA_BUF) {
+	// Enable DMA peripheral
+	RCC->AHBPCENR |= RCC_AHBPeriph_DMA1;
+
+	// Enable Tx DMA
+	SPI1->CTLR2 |= SPI_I2S_DMAReq_Tx;
+
+	// Configure DMA
+	DMA_Channel->PADDR = (uint32_t)&SPI1->DATAR;
+	// DMA1_Channel3->MADDR = *DATA_BUF;
+	DMA_Channel->CFGR = DMA_M2M_Disable |
+						DMA_Priority_VeryHigh |
+						DMA_MemoryDataSize_Byte |
+						DMA_PeripheralDataSize_Byte |
+						DMA_MemoryInc_Enable |
+						DMA_PeripheralInc_Disable |
+						DMA_Mode_Circular |
+						DMA_DIR_PeripheralDST;
+}
+
+static void SPI_send_DMA(const uint8_t* buffer, uint16_t len) {
+	FN_SPI_DC_HIGH();
+	
+	DMA1_Channel3->CNTR  = len;
+	DMA1_Channel3->MADDR = (uint32_t)buffer;
+
+	// Start DMA transfer
+	DMA1_Channel3->CFGR |= DMA_CFGR1_EN;  
+
+	DMA1->INTFCR = DMA1_FLAG_TC3;
+	while (!(DMA1->INTFR & DMA1_FLAG_TC3));
+
+	// Stop DMA transfer
+	DMA1_Channel3->CFGR &= ~DMA_CFGR1_EN;
+}
+
+//# write read raw
+static inline uint8_t SPI_read_8() {
+	return SPI1->DATAR;
+}
+static inline uint16_t SPI_read_16() {
+	return SPI1->DATAR;
+}
+static inline void SPI_write_8(uint8_t data) {
+	SPI1->DATAR = data;
+}
+static inline void SPI_write_16(uint16_t data) {
+	SPI1->DATAR = data;
+}
+
+static inline void SPI_wait_TX_complete() {
+	while(!(SPI1->STATR & SPI_STATR_TXE)) {}
+}
+
+void SPI_end() {
+	SPI1->CTLR1 &= ~(SPI_CTLR1_SPE);
+}
+
+uint8_t SPI_transfer_8(uint8_t data) {
+	SPI_write_8(data);
+	while (!(SPI1->STATR & SPI_STATR_TXE));
+	asm volatile("nop");
+	while (!(SPI1->STATR & SPI_STATR_RXNE));
+	return SPI_read_8();
+}
+
+uint16_t SPI_transfer_16(uint16_t data) {
+	SPI_write_16(data);
+	while (!(SPI1->STATR & SPI_STATR_TXE));
+	asm volatile("nop");
+	while (!(SPI1->STATR & SPI_STATR_RXNE));
+	return SPI_read_16();
+}
---
+++ examples_x035/spi_st7735_test/spi_st7735_test.c
@@ -0,0 +1,57 @@
+// Simple example to show PWM output on TIM2 Channel 4 (PA3)
+// Connect an LED to PA3 to see the PWM signal output
+
+#include "ch32fun.h"
+#include <stdio.h>
+#include "lib_st7735.h"
+
+// R32_SPI0_CONTROL
+// PA13:	SCK
+// PA14:	MOSI
+
+// R32_SPI1_CONTROL
+// PA0: 	SCK1
+// PA1: 	MOSI1
+
+int main() {
+	SystemInit();
+	Delay_Ms(100);
+	funGpioInitAll();
+
+	SPI_Pins_t spi_pins = {
+		.mosi_pin = PA7,
+		.miso_pin = PA6,
+		.sck_pin = PA5,
+		.cs_pin = -1,
+		.dc_pin = PA10,
+		.rst_pin = PA1
+	};
+
+	printf("~SPI ST7735 TEST 2~\n");
+	fun_st7335_init(160, 80, &spi_pins);
+	tft_fill_rect(0, 0, 160, 80, ST_PURPLE);
+
+	char str[25] = {0};
+	int counter = 0;
+	
+
+	// while(1) {
+	// 	SPI_cmd_8(ST7735_COLMODE);
+	// 	// SPI_data_8(0x05); // 0x03: 12-bit, 0x05: 16-bit, 0x06: 18-bit, 0x07: Not used
+
+	// 	// fun_st7335_init(160, 80, &spi_pins);
+	// 	// tft_fill_rect(0, 0, 160, 80, ST_PURPLE);
+	// 	// sprintf(str, "Hello Bee %d", counter++);
+	// 	// tft_print(str, 0, 0, ST_WHITE, ST_PURPLE);
+	// 	Delay_Ms(1000);
+	// }
+
+	while(1) {
+		funDigitalWrite(SPI_PINS->dc_pin, 1);
+		Delay_Ms(1000);
+		funDigitalWrite(SPI_PINS->dc_pin, 0);
+		Delay_Ms(1000);
+		SPI1->DATAR = ST7735_COLMODE;
+		Delay_Ms(1000);
+	}
+}
